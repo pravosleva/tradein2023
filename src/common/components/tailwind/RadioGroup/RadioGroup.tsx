4a5bf2c954da18ff93e089e7400f016a0aa6d758
Den <selection4test@gmail.com>
2023-11-28 00:06:08 +0300

    ui fix

--- src/common/components/tailwind/RadioGroup/RadioGroup.tsx
+++ src/common/components/tailwind/RadioGroup/RadioGroup.tsx
@@ -181,13 +181,15 @@ function CheckIcon(props: React.SVGAttributes<SVGElement>) {
     <svg viewBox='0 0 24 24' fill='none' {...props}>
       <circle
         cx={12} cy={12} r={11}
-        // fill='#fff' opacity='0.2'
+        // fill='#fff'
+        fill='#3882c4'
+        // opacity='0.2'
         strokeWidth='2'
       />
       <path
         d='M7 13l3 3 7-7'
-        // stroke='#fff'
-        strokeWidth={1.5}
+        stroke='#fff'
+        strokeWidth={2}
         strokeLinecap='round'
         strokeLinejoin='round'
       />
